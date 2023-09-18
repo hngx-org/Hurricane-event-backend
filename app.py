c5b212b1549b20fa1e076e733bebdb71a919204f
Adooz <kndonake@gmail.com>
2023-09-19 00:50:23 +0100

    Title: Database initialization and configuration

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Initialize the Flask application
 app = Flask(__name__)
 
+# Configure the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Create a SQLAlchemy database instance
+db = SQLAlchemy(app)
+
+# Define a model(Test Purpose) 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+   
+
+# Create the database tables (you need to call this only once)
+with app.app_context():
+    db.create_all()
+
+# This code initializes a Flask app, configures the SQLite database, creates a SQLAlchemy database instance,
+# and defines a simple "User" model. The "SQLALCHEMY_DATABASE_URI" specifies the SQLite database file,
+# and "SQLALCHEMY_TRACK_MODIFICATIONS" disables Flask-SQLAlchemy modification tracking to save resources.
+
+# You can create models and modify the database as needed using the SQLAlchemy ORM.
 
 
-if __name__ == '__main__':
-    app.run()
