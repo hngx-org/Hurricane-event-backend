2fc6dd13ffd548a76a3a832c25c8d347ca22006e
Tony Eseh <esehtony123@gmail.com>
2023-09-19 09:33:26 +0100

    init db to flask app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
+from db_connection.connection import db
+from database import models
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
+db.init_app(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
