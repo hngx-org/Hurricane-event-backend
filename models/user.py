d173a06df28174881f13ef8db5d0eb19213bd4e2
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-09-19 23:10:48 +0100

    Implemented the User model

---
+++ models/user.py
@@ -0,0 +1,30 @@
+"""Model of the User class"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.basemodel import BaseModel, Base
+from models.group import user_groups
+from models.event import interested_events
+
+
+class User(BaseModel, Base):
+    """User Class"""
+
+    __tablename__ = "users"
+    name = Column(String(120))
+    email = Column(String(120), unique=True)
+    access_token = Column(String(255))
+    refresh_token = Column(String(255))
+    avatar = Column(String(255), nullable=True)
+    groups = relationship("Group", secondary=user_groups,
+                          back_populates="users")
+    events = relationship("Event", secondary=interested_events,
+                          back_populates="users")
+
+    def __init__(self, name: str, email: str, access_token: str,
+                 refresh_token: str, avatar: str):
+        """Initializes the User class"""
+        self.name = name
+        self.email = email
+        self. access_token = access_token
+        self.refresh_token = refresh_token
+        self.avatar = avatar
