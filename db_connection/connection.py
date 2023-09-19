011162510530c569802d6dff85e98e4590295797
Adooz <kndonake@gmail.com>
2023-09-19 16:54:42 +0100

    restored connection.py to initial state

--- db_connection/connection.py
+++ db_connection/connection.py
@@ -1,7 +1,1 @@
-# Database connection script
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Adjust the database URI as needed
-engine = create_engine('sqlite:///event_app.db')
-Session = sessionmaker(bind=engine)
+# Database connection scriptz
