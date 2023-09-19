97c6be07a25ba52e5772f24022e9cd2768c22173
CyberPraise <64874415+praiseordu@users.noreply.github.com>
2023-09-19 23:30:41 +0100

    Update forms.py

--- events/forms.py
+++ events/forms.py
@@ -1,1 +1,15 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    profile_pic = db.Column(db.String(255))
+
+    def __init__(self, email, name, password, profile_pic=None):
+        self.email = email
+        self.name = name
+        self.password = generate_password_hash(password, method='sha256')
+        self.profile_pic = profile_pic
 # For handling user input validation
