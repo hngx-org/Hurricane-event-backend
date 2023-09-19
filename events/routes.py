b8a7500dd58edaaa44ec4bc45a9e18a862e31ad0
CyberPraise <64874415+praiseordu@users.noreply.github.com>
2023-09-19 23:31:18 +0100

    Update routes.py

--- events/routes.py
+++ events/routes.py
@@ -1,1 +1,34 @@
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import app, db
+from .models import User
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+
+        # Check if the email is already in use
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('Email already in use', 'error')
+            return redirect(url_for('signup'))
+
+        # Create a new user
+        new_user = User(email=email, name=name, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Account created successfully', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+# You can define other routes and views here, such as a login route
 # Routes for handling event related functionality (event creation, updating and deleting)
