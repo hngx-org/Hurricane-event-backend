1c7645372808bccb3c0bb2a99588f3e2128d8fd2
Tony Eseh <esehtony123@gmail.com>
2023-09-19 08:10:21 +0100

    created config class with sql_uri

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 # Application's configuration settings
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    #SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
+        or 'sqlite:///' + os.path.join(basedir, 'event_app.db')
