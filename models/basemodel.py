f194f6902547a7f9f676a8e5e56344d0ea13d821
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-09-19 23:09:07 +0100

    Implemented the basemodel

---
+++ models/basemodel.py
@@ -0,0 +1,26 @@
+"""Base Model of the application"""
+from uuid import uuid4
+
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class BaseModel:
+    """BaseModel Class"""
+
+    id = Column(String(60), primary_key=True, default=str(uuid4()))
+
+    def to_dict(self):
+        """Converts object's properties to dict object
+
+        Returns:
+            dict: Dictionary representation of object
+        """
+        new_dict = self.__dict__.copy()
+
+        if "_sa_instance_state" in new_dict:
+            new_dict.pop("_sa_instance_state")
+
+        return new_dict
