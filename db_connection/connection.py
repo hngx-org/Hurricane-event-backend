d84aa0d36063e98a30d398be0d0be057cf7feba9
phurhard <phurhardeen@gmail.com>
2023-09-19 11:39:42 +0100

    Update connection.py

--- db_connection/connection.py
+++ db_connection/connection.py
@@ -1,10 +1,1 @@
-# Database connection script
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    """declarative base"""
-    pass
-
-db = SQLAlchemy(model_class=Base)
+# DB connection script
