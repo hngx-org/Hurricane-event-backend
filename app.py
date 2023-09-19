9b5a2a605372019f9a7d009b663b2c7cf2e139d4
Godwin987 <basseygodwin911@gmail.com>
2023-09-19 10:34:47 +0100

    Connect db event_app.db

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from database import db
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello_world():  # put application's code here
