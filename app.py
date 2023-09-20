c75b1913f48953355613d521b920600e936e8ffc
Stoney <chisomd90@gmail.com>
2023-09-20 23:05:34 +0100

    Add additional feature or fix issue #35

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask
-from config import Config
-from db_connection.connection import db
+# from config import Config
+# from db_connection.connection import db
 import models
 
 app = Flask(__name__)
 
-app.config.from_object(Config)
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
+# app.config.from_object(Config)
+# db.init_app(app)
 
 
-    @app.route('/')
-    def hello_world():  # put application's code here
-        return 'Hello World!'
+@app.teardown_appcontext
+def close_database():
+    """Loads data into session from database"""
+    models.storage.close()
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
