fa9516fed8085bb43f97fc4a2e283d087cd6497d
phurhard <phurhardeen@gmail.com>
2023-09-20 13:07:44 +0100

    Important imports

--- events/routes.py
+++ events/routes.py
@@ -1,1 +1,8 @@
+from models import storage # storage will be used for all db session based queries
+from models.user import User
+from models.comment import Comment
+from models.event import Event
+from models.interested_event import InterestedEvent
 # Routes for handling event related functionality (event creation, updating and deleting)
+""" This are the required imports do not change them and if you must
+import something else be sure to specify it in your documentation reason you had to import"""
