52d4f2da0bb30757dedda30b79dee0f721f93253
Stoney <chisomd90@gmail.com>
2023-09-20 21:24:21 +0100

    Fix issue #35: Created an Api that lists the groups a users belongs to, by using userid

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -9,3 +9,19 @@ To run the tests, execute the following line of code:
 `pytest`
 
 """
+# tests/test_api.py
+
+import requests
+
+def test_get_user_groups():
+    # Replace 'http://localhost:5000' with the actual URL of your API
+    base_url = 'http://127.0.0.1:5000'
+    user_id = 1
+    endpoint = f'{base_url}/api/{user_id}/groups'
+
+    response = requests.get(endpoint)
+
+    assert response.status_code == 200
+    data = response.json()
+    assert 'user_id' in data
+    assert 'groups' in data
