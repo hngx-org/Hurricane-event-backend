b6c34758f537fd1701c12d18b17de5adfb423a04
Your Name <daveed925@gmail.com>
2023-09-20 21:32:30 +0100

    Update: Added the Endpoint to GET all comments for an event

--- events/routes.py
+++ events/routes.py
@@ -1,3 +1,4 @@
+from flask import Blueprint, abort
 import models # storage will be used for all db session based queries
 from models.user import User
 from models.comment import Comment
@@ -10,3 +11,26 @@ from models.interested_event import InterestedEvent
 something else be sure to specify it in your documentation reason you had to
 import
 """
+
+event = Blueprint('event', __name__)
+
+def format_output(comment):
+    return {
+        'id': comment.id,
+        'body': comment.body,
+        'event_id': comment.event_id
+        }
+
+@event.route('/api/events/<int:event_id>/comments', methods=['GET'])
+def retrieve_comments_by_event(event_id):
+    try:
+        comments = Comment.query.filter(Comment.event_id == event_id).all()
+        return jsonify(
+                {
+                    'success': True,
+                    'comments': [format_output(comment) for comment in comments]
+                    }
+                )
+    except SQLAlchemy as e:
+        models.storage.session.rollback()
+        abort(404)
