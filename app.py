cebc3c4610dc3ac8164a303af92f864ead43c5df
Adooz <kndonake@gmail.com>
2023-09-20 01:39:59 +0100

    an endpoint to GET a user

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from config import Config
 from db_connection.connection import db
 import models
+from api.routes import api
+import models
 
 app = Flask(__name__)
+app.register_blueprint(api, url_prefix='/api')
 
 app.config.from_object(Config)
 
