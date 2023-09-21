2073b6125c8843ac2e569341abb20c3d4a4780b4
Zainab M <76655241+zee467@users.noreply.github.com>
2023-09-21 06:34:28 +0100

    an endpoint that gets a group's detail

---
+++ get_details.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+class Group:
+    pass
+
+MAX_NO_OF_IMAGES = 2
+
+def to_dict(obj):
+    data = {}
+    for field in obj.__table__.columns:
+        value = getattr(obj, field.name)
+        if isinstance(value, set):
+            # Convert sets to lists 
+            data[field.name] = list(value)
+        else:
+            data[field.name] = value
+    return data
+
+
+@app.route("/api/groups/<group_id>", methods=["GET"])
+def group_details(group_id):
+    # check if a group exists with this id
+    group = Group.query.filter_by(id=group_id).first()
+    if not group:
+        return jsonify({"error": "Group doesn't exist!"}), 404
+
+    group_details = {
+        "name": group.title,
+        "events": [to_dict(event) for event in group.events],
+        "images": [to_dict(image) for image in group.images[:MAX_NO_OF_IMAGES]],
+        "users": [to_dict(user) for user in group.users]}
+    
+    return jsonify({"details": group_details}), 200
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
