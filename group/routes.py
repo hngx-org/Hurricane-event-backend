20665261df145caa5583d752a85f7a76263ef909
phurhard <phurhardeen@gmail.com>
2023-09-21 03:50:48 +0100

    Doccumentation on the PR is available at hange_logs.md Please read

--- group/routes.py
+++ group/routes.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, request, jsonify
 from models.group import Group  # Import the Group model
+from models.user import User
 import models
 
 # Create a Blueprint for group-related routes
-group_bp = Blueprint('group', __name__)
+group_bp = Blueprint('groups', __name__)
 
 # Route to create a new group
-@group_bp.route('/api/groups', methods=['POST'])
+@group_bp.route('/groups', methods=['POST'])
 def create_group():
     data = request.get_json()
 
@@ -24,19 +25,18 @@ def create_group():
     try:
         # Commit the changes to the database
         new_group.save()
-        response_data = {'id': new_group.id, 'title': new_group.title}
-        return jsonify(response_data), 201
+        return jsonify(new_group.json()), 201
     except Exception as e:
         # Handle database errors
         models.storage.rollback()
         return jsonify({'error': str(e)}), 500
 
-@group_bp.route('/api/groups/<groupId>', methods=['PUT'])
+@group_bp.route('/groups/<groupId>', methods=['PUT'])
 def update_group(groupId):
     '''This function updates the details of a
     group.
 
-    Args: groupId
+    Args: groupId, new_name of the group
     Returns the updated group details'''
 
     if request.json():
@@ -46,4 +46,28 @@ def update_group(groupId):
         group.title = title
         group.save()
 
-    return jsonify(group.json())
+        return jsonify(group.json())
+    else:
+        return jsonify({"Error", "Invalid parameters"}), 403
+
+@group_bp.route('/groups/<userid>/groups', methods=['GET'])
+def get_user_groups(userid):
+    """Gets all the groups a user is part of
+
+    Args:
+        userid (str | uuid): The user I used to deterrmine which
+        user groups are requested for
+
+    Returns:
+        JSON | str: Returns all the groups a user is part of
+    """
+    user = models.storage.get(User, userid)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    all_groups = models.storage.all(Group)
+    groups = [group for group in all_groups if group.user_id == userid]
+    group_list = [{'id': group.id, 'name': group.name} for group in groups]
+
+    return jsonify({'user_id': userid, 'groups': group_list}), 200
+
