81cd0544cddb3a8d3b3cd4d94783648650f52c80
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-21 18:45:15 +0100

    CRUD operations: User resource

--- app.py
+++ app.py
@@ -19,17 +19,15 @@ def close_database(exception):
     models.storage.close()
 
 
-# status check
 @app.route('/')
 def hello_world():  # put application's code here
     return jsonify({"message": "Hello World!"})
 
 
-# Register the blueprints for each package here
 app.register_blueprint(group_bp, url_prefix='/groups')
 app.register_blueprint(event_bp, url_prefix='/events')
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(api, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
