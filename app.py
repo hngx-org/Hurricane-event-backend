0ee200c8893cbb4ed3efac667c6325658eef01d4
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-09-21 14:42:50 +0100

    Fixed all issues with app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from group.routes import group_bp
-from events.routes import event_bp
+from events.routes import event
 from auth.routes import auth
+from api.routes import api
 # from config import Config
 # from db_connection.connection import db
 import models
@@ -13,7 +14,7 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_database():
+def close_database(exception):
     """Loads data into session from database"""
     models.storage.close()
 
@@ -24,8 +25,8 @@ def hello_world():  # put application's code here
 
 # Register the blueprints for each package here
 app.register_blueprint(group_bp, url_prefix='/groups')
-app.register_blueprint(event_bp, url_prefix='/events')
-app.register_blueprint(auth, url_prefix='auth')
+app.register_blueprint(event, url_prefix='/events')
+app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(api, url_prefix='/api')
 
 if __name__ == '__main__':
