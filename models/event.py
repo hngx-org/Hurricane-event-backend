10ec8faa40f30d7b10d53b9f1186b267b9e90166
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-09-22 03:47:48 +0100

    Fixed typo

--- models/event.py
+++ models/event.py
@@ -21,16 +21,16 @@ class Event(BaseModel, Base):
     start_time = Column(Time)
     end_time = Column(Time)
     creator_id = Column(String(60), ForeignKey("users.id"))
+    thumbnail = Column(String(60), nullable=True)
     groups = relationship("Group", secondary=group_events,
                           back_populates="events")
     users = relationship("User", secondary=interested_events,
                          back_populates="events")
-    image = Column(String(60), nullable=True)
     # image = relationship("Image", secondary=event_thumnail, uselist=False)
 
     def __init__(self, title: str, description: str, location: str,
                  start_date: str, end_date: str, start_time: str,
-                 end_time: str, thumnail: str, creator_id: str):
+                 end_time: str, thumbnail: str, creator_id: str):
         """Initializes the event class"""
         self.title = title
         self.description = description
@@ -39,7 +39,7 @@ class Event(BaseModel, Base):
         self.end_date = date.fromisoformat(end_date)
         self.start_time = time.fromisoformat(start_time)
         self.end_time = time.fromisoformat(end_time)
-        self.thumbnail = thumnail
+        self.thumbnail = thumbnail
         self.creator_id = creator_id
 
         super().__init__()
