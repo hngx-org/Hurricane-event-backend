acefdbeba2026d74fce4a48b59924bd8c191b03d
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-09-22 06:42:07 +0100

    Implemented the Group endpoints

--- views/group.py
+++ views/group.py
@@ -9,6 +9,7 @@ def create_group():
     """Creates a new group"""
     data = request.get_json()
     title = data.get("title")
+    image = data.get("image")
 
     # Check if 'title' is provided in the JSON data
     if not title:
@@ -23,6 +24,8 @@ def create_group():
 
     # Create a new group instance
     new_group = Group(title=title)
+    if image:
+        new_group.update(image=image)
     # Commit the changes to the database
     new_group.save()
     return jsonify(new_group.to_dict()), 201
@@ -39,25 +42,64 @@ def get_group(group_id):
     return jsonify(group.to_dict()), 200
 
 
-@api_views.route("/groups/<group_id>")
+@api_views.route("/groups/<group_id>", methods=["PUT"])
 def update_group(group_id):
     """Updates a group resource"""
-    pass
+    data = request.get_json()
+    title = data.get("title")
+    image = data.get("image")
+
+    group = models.storage.get("Group", group_id)
+    if group:
+        kwargs = {}
+        if title:
+            kwargs["title"] = title
+        if image:
+            kwargs["image"] = image
+        if kwargs:
+            group.update(**kwargs)
+            group.save()
+            return jsonify({"message": "success"}), 202
+        return jsonify({"message": "unchanged"})
+    return jsonify({"message": "Invalid Group ID"}), 404
 
 
-@api_views.route("/groups/<group_id>")
+@api_views.route("/groups/<group_id>", methods=["DELETE"])
 def delete_group(group_id):
     """Deletes a group resource"""
-    pass
+    group = models.storage.get("Group", group_id)
+    if group:
+        models.storage.delete("Group", group_id)
+        models.storage.save()
+        return jsonify({"message": "success"})
+    return jsonify({"message": "Invalid Group ID"}), 404
 
 
 @api_views.route("/groups/<group_id>/members/<user_id>", methods=["POST"])
 def add_user(group_id, user_id):
     """Adds a user to the group"""
-    pass
+    group = models.storage.get("Group", group_id)
+    user = models.storage.get("User", user_id)
+
+    if not user:
+        return jsonify({"message": "Invalid User ID"}), 404
+    if not group:
+        return jsonify({"message": "Invalid Group ID"}), 404
+    group.users.append(user)
+    group.save()
+    return jsonify({"message": "success"})
 
 
 @api_views.route("/groups/<group_id>/members/<user_id>", methods=["DELETE"])
 def remove_user(group_id, user_id):
     """Removes a user from group"""
-    pass
+    group = models.storage.get("Group", group_id)
+    user = models.storage.get("User", user_id)
+
+    if not user:
+        return jsonify({"message": "Invalid User ID"}), 404
+    if not group:
+        return jsonify({"message": "Invalid User ID"}), 404
+    group.users.remove(user)
+    group.save()
+    return jsonify({"message": "success"})
