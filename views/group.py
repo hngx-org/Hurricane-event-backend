520632c41b519aa5c70a79809878b5a382e9f5ca
wambuiwambui <62937590+wambuiwambui@users.noreply.github.com>
2023-09-22 04:17:03 +0300

    Update group.py views - Create group endpoint

--- views/group.py
+++ views/group.py
@@ -1,3 +1,4 @@
+from flask import request, jsonify
 import models
 from . import api_views
 from models.group import Group
@@ -6,7 +7,33 @@ from models.group import Group
 @api_views.route("/groups", methods=["POST"])
 def create_group():
     """Creates a new group"""
-    pass
+    data = request.get_json()
+
+    # Check if 'title' is provided in the JSON data
+    if 'title' not in data:
+        return jsonify({'error': 'Title is required'}), 400
+
+    title = data['title']
+
+    all_groups = models.storage.all(Group)
+
+    title_exists = [group for group in all_groups if group.title == title]
+
+    if title_exists:
+        return jsonify({'error': 'Title already exists'}) 
+
+    # Add the new group to the database session
+
+    try:
+        # Create a new group instance
+        new_group = Group(title=title)
+        # Commit the changes to the database
+        new_group.save()
+        return jsonify(new_group.to_dict()), 201
+    except Exception as e:
+        # Handle database errors
+        # models.storage.rollback()
+        return jsonify({'error': str(e)}), 500
 
 
 @api_views.route("/groups/<group_id>")
