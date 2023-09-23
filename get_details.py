810bcda64801ee3863c1910eacad641a75f8cc17
phurhard <phurhardeen@gmail.com>
2023-09-23 06:30:32 +0100

    Update get_details.py
    Edited in place to make it functional, and moved it to view/group.py

--- get_details.py
+++ get_details.py
@@ -2,10 +2,7 @@ from flask import Flask, jsonify
 from models.group import Group
 
 app = Flask(__name__)
-
-
 MAX_NO_OF_IMAGES = 2
-
 def to_dict(obj):
     data = {}
     for field in obj.__table__.columns:
@@ -16,10 +13,6 @@ def to_dict(obj):
         else:
             data[field.name] = value
     return data
-
-
-
-
 @app.route("/api/groups/<group_id>", methods=["GET"])
 
 """"
@@ -29,13 +22,14 @@ def group_details(group_id):
     group = models.storage.get("Group", group_id)
     if not group:
         return jsonify({"error": "Group doesn't exist!"}), 404
-    #group_events = group.events
+    group_events = group.events
+    user_groups = group.users
 
     group_details = {
         "name": group.title,
-        "events": [to_dict(event) for event in group.events],
-        "images": [to_dict(image) for image in group.images[:MAX_NO_OF_IMAGES]],
-        "users": [to_dict(user) for user in group.users]}
+        "events": [event.to_dict() for event in group_events],
+        "users": [user.to_dict() for user in user_groups]
+        }
     
     return jsonify({"details": group_details}), 200
 """
